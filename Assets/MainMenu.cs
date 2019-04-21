080e8d6398d9a0dbcebc7e23e4ed77236d11b69c
Jacob S <bloggingpenguin@gmail.com>
2019-04-21 17:26:28 -0400

    Added Menus
    Added main menu, options menu with options to change volume, resolution, and fullscreen.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+    public void play() {
+        //load the next scene in the queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quit() {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start() {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        //create a list of avaiable resolutions that the game can run at
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        //Add resolutions to the list
+        for(int i = 0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width == Screen.currentResolution.width && 
+               resolutions[i].height == Screen.currentResolution.height) {
+                    currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+
+    public void setResolution(int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void setVolume(float volume) {
+        //change the volume of the game's audio
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void setQuality(int qualityIndex) {
+        //change the quality of the game's visuals
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullscreen(bool fullScreen) {
+        //Set fullscreen on or off
+        Screen.fullScreen = fullScreen;
+    }
+}
